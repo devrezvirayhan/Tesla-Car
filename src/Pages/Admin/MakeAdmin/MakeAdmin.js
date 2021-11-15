d03562e5d816d4d6b7764011aac05016ae5c23c0
rezvirayhan2002 <rezvirayhan2002@gmail.com>
2021-11-15 17:15:09 +0600

    admin done

--- src/Pages/Admin/MakeAdmin/MakeAdmin.js
+++ src/Pages/Admin/MakeAdmin/MakeAdmin.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import { Alert } from '@mui/material';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import React, { useState } from 'react';
+
 
 const MakeAdmin = () => {
+    const [email, setEmail] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    const handleOnBlur = e => {
+        setEmail(e.target.value);
+    }
+    const handleAdminSubmit = e => {
+        const user = { email };
+        fetch('http://localhost:5000/users/admin', {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                    console.log(data);
+                
+            })
+
+        e.preventDefault()
+    }
     return (
         <div>
-            <h2>This Is Admin Pagess</h2>
+            <h2>Make an Admin</h2>
+            <form onSubmit={handleAdminSubmit}>
+                <TextField
+                    sx={{ width: '50%' }}
+                    label="Email"
+                    type="email"
+                    onBlur={handleOnBlur}
+                    variant="standard" /> <br/>
+                <Button className="mt-2" type="submit" variant="contained">Make Admin</Button>
+            </form>
+            {success && <Alert severity="success">Make Admin successfully!</Alert>}
         </div>
     );
 };
